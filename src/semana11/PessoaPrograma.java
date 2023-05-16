8aa03039d6340ecd9b79633c7dc2a2bfdf7f3b85
Guilherme Yoshida <126599207+gyyoshida@users.noreply.github.com>
2023-05-16 09:33:47 -0300

    Update PessoaPrograma.java

--- src/semana11/PessoaPrograma.java
+++ src/semana11/PessoaPrograma.java
@@ -4,7 +4,7 @@ public class PessoaPrograma {
     Pessoa p1 = new Pessoa();
     p1.altura = 172;
     p1.peso = 70.3;
-
+    
     System.out.println(p1.calcularImc());
     System.out.println(p1.getFaixa());
     System.out.println(p1.getSituacao());
