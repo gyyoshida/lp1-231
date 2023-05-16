e96201fd5e5ad245be101feaa3ad1cf93321ea2e
Guilherme Yoshida <126599207+gyyoshida@users.noreply.github.com>
2023-05-16 09:29:42 -0300

    Update Pessoa.java

--- src/semana11/Pessoa.java
+++ src/semana11/Pessoa.java
@@ -6,20 +6,31 @@ public class Pessoa {
 
     public double calcularImc() {
         return peso / Math.pow(altura, 2);
-
     }
 
     public String getFaixa() {
-        
+        if (calcularImc() < 18.5) {
+            return "ABAIXO DO PESO";
+        } else 
+            if (calcularImc() < 25) {
+              return "PESO NORMAL";
+            } else 
+                if (calcularImc() < 30) {
+                 return "SOBREPESO";
+                } else {
+                     return "OBESIDADE";
+                       }  
     }
+    
 
     public String getSituacao() {
-        if (calcularImc() > 24.9) {
-            return "PERDER";
-        } else
-            if (calcularImc() < 18.5) {
-                return "GANHAR";
-            } else 
+        if (calcularImc() < 18.5) {
+            return "GANHAR"; 
+        } else 
+            if (calcularImc() < 25) {
                 return "NORMAL";
+            } else {
+                    return "PERDER"; 
+                   }
     }
 }
