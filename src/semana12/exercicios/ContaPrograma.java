c329ad6e19fdb0f6f56e2923b4c04fc2bcd2fd2e
Guilherme Yoshida <guiyyoshida@gmail.com>
2023-05-16 17:16:16 -0300

    finalização vf

--- src/semana12/exercicios/ContaPrograma.java
+++ src/semana12/exercicios/ContaPrograma.java
@@ -3,16 +3,13 @@ package semana12.exercicios;
 public class ContaPrograma {
     public static void main(String[] args) {
 
-        Conta conta1 = new Conta(1, "Yuji");
-        Conta conta2 = new Conta(2, "Yoshi");
+        Conta conta1 = new Conta(1, "Guilherme");
+        Conta conta2 = new Conta(2, "Yoshida");
         
-        System.out.println(conta1.depositar(90));
-        System.out.println(conta1.sacar(40));
-        System.out.println(conta1.transferirValores(conta2, 500));
-
-        System.out.println(conta2.depositar(100));
-        System.out.println(conta2.sacar(400));
-        System.out.println(conta2.transferirValores(conta1, 1000));
-
+        System.out.println(conta1.correntista + " depositou 90 | Saldo da conta:" + conta1.depositar(90));
+        System.out.println(conta1.correntista + " sacou 40 | Saldo da conta: " + conta1.sacar(40));
+        System.out.println(conta1.correntista + " transferiu 500 para " + conta2.correntista + " | Saldo da conta: " + conta1.transferirValores(conta2, 500));
+        System.out.println(conta2.correntista + " transferiu 1000 para " + conta1.correntista + " | Saldo da conta: " + conta2.transferirValores(conta1, 1000));
+    
     }
 }
